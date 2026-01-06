6a299fdd1c673516b99b88ebb4a58abdec8c2580
Kevin Kupp <kevin.kupp@voco.ee>
2026-01-06 13:41:57 +0200

    Feature-A: Show a greeting message to user and ask for his/her name

---
+++ welcome.py
@@ -0,0 +1,9 @@
+from get_name import *
+
+print("="*35)
+print("Welcome to voting eligibility check. It's nothing to be scared of.")
+print("="*35)
+print("\n")
+
+user_name = get_name()
+print(f"Hi, {user_name}!\n")
