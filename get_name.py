081b1b3e6576cf15413476478ec60f213e49e17e
Sten-TõnisFeldmann <sten-tonis.feldmann@voco.ee>
2026-01-07 12:38:28 +0200

    Add files via upload

---
+++ get_name.py
@@ -0,0 +1,3 @@
+def get_name():
+    name = input("Please enter your name: ")
+    return name
---
+++ validate_age.py
@@ -0,0 +1,5 @@
+def validate_age(age):
+    if age < 18:
+        print("Not eligible for voting\n\n")
+    else:
+        print("Eligiable for voting\n\n")
